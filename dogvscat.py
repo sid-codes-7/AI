45a78dc44dd6ccd0c74640ec1399feb3a12510cf
shinyfires <120816320+sid-codes-7@users.noreply.github.com>
2026-07-23 11:20:21 -0400

    Implement model file search in directory
    Added functionality to search for saved model files in the specified directory.

--- dogvscat.py
+++ dogvscat.py
@@ -19,3 +19,16 @@ MODEL_DIRECTORY = (
 
 # Search all Kaggle datasets for the individual cat and dog images
 IMAGE_DIRECTORY = "/kaggle/input/datasets"
+
+# ============================================================
+# 2. FIND THE SAVED MODEL
+# ============================================================
+
+model_paths = []
+
+#get the complete location of that file and save it into model_paths
+for root, folders, files in os.walk(MODEL_DIRECTORY):
+    for filename in files:
+        if filename.lower().endswith((".keras", ".h5")):
+            full_path = os.path.join(root, filename)
+            model_paths.append(full_path)
